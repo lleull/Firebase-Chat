523125a7fefc5b1a4c38d4ebb82c6ccc89a2a97f
lleull <leulmekonnen049@gmail.com>
2024-07-27 10:30:03 +0300

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Chat from "./components/chat/Chat"
 import Details from "./components/details/Details"
 import List from "./components/list/List"
+import Login from "./components/login/login"
 const App = () => {
+
+  const user = true
   return (
     <div className='container'>
+      {user ? 
+        <>
       <List />
       <Chat />
       <Details/>
+        </>
+    : <Login/>}
     </div>
   )
 }
