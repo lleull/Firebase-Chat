024b1b01f370ea75f761a1581726f8b9e8b96ddd
lleull <leulmekonnen049@gmail.com>
2024-07-29 16:15:59 +0300

    Registred

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
         file: null,
         url: ""
     })
+    const [loading, setLoading] = useState(false)
     const handleImage = (e) => {
         if (e.target.files[0]) {
             
@@ -23,9 +24,6 @@ const Login = () => {
         }
     }
 
-    useEffect(() => {
-        console.log("AVAVAV", avatar)
-    },[avatar])
 
     const handleLogin = async(e) => {
 
@@ -39,13 +37,16 @@ const Login = () => {
 
     }
     const handleRegister = async(e) => {
-        e.preventDefault()
+        e.preventDefault();
         const formData = new FormData(e.target)
         
         const { username, email, password } = Object.fromEntries(formData)
         try {
+            setLoading(true)
             const res = await createUserWithEmailAndPassword(auth, email, password)    
+            
             const imgUrl = await upload(avatar.file)
+            console.log("image", imgUrl)
         
             await setDoc(doc(db, "users", res?.user?.uid), {
                 username,
@@ -56,13 +57,16 @@ const Login = () => {
             })
             await setDoc(doc(db, "userchats", res?.user?.uid), {
                 chats:[]
-              })
-                
+            })
+            
             toast.success("Accout created! you can now login")
             
         } catch (error) {
             console.log("Errr", error?.message)
             // toast.error(`${Error.toString()}`)
+            
+        } finally {
+            setLoading(false)
             
         }
 
@@ -90,7 +94,7 @@ const Login = () => {
                   <input type="text" placeholder='Username' name='username' />
                   <input type="text" placeholder='Email' name='email' />
                   <input type="text" placeholder='Password' name='password' />
-                  <button>Sign Up</button>      
+                  <button disabled={loading}>{loading ? "Loading" : "Sign Up"}</button>      
               </form>
               
           </div>
--- src/lib/upload.js
+++ src/lib/upload.js
@@ -1,13 +1,10 @@
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-
+import { storage } from "./firebase";
 const upload = async (file) => {
-    const storage = getStorage(file.name);
-    
-    
+
     const date = new Date()
     
-    const storageRef = ref(storage, `images/${date}` + file.name);
-    console.log("wwwww", storageRef)
+    const storageRef = ref(storage, `images/${date + file.name}`);
     const uploadTask = uploadBytesResumable(storageRef, file);
     
     return new Promise((resolve, reject) => {
@@ -15,6 +12,7 @@ const upload = async (file) => {
         
         uploadTask.on('state_changed',
             (snapshot) => {
+                console.log("SSNSNSNNNAP",snapshot)
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                 console.log('Upload is ' + progress + '% done');
             }, 
